7f1b6f3c4d0d98e082fc5241868c6a1e4c07a2af
Dmitry_Malyasov <dmalyasov24@gmail.com>
2023-02-02 17:51:08 +0700

    task6 done

---
+++ task6.py
@@ -0,0 +1,10 @@
+def isMyHappinessClose(number):
+    if (int(number[0]) + int(number[1]) + int(number[2]) == int(number[3]) + int(number[4]) + int(number[5])):
+        print("yes")
+    else:
+        print("no")
+
+number = str(input())
+isMyHappinessClose(number)
+
+
